aef11384894c99bfa2d2cb6ca4f1d84c991f211f
BPJ <zh.sh.woo@gmail.com>
2025-09-30 13:53:22 +0800

    新增：移除侧边拦

--- web_scripts/ithome-remove-menu.user.js
+++ web_scripts/ithome-remove-menu.user.js
@@ -1,22 +1,28 @@
 // ==UserScript==
-// @name         IT之家-移除软媒应用菜单
+// @name         IT之家 界面净化
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  移除IT之家网站顶部导航栏中的“软媒应用”菜单。
+// @version      0.2
+// @description  移除IT之家网站的“软媒应用”菜单和右侧悬浮功能栏，提供更纯净的阅读体验。
 // @author       Gemini
 // @match        https://*.ithome.com/*
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/bpjanson/Vibe_Coding/main/web_scripts/ithome-remove-menu.user.js
+// @downloadURL  https://raw.githubusercontent.com/bpjanson/Vibe_Coding/main/web_scripts/ithome-remove-menu.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // 通过ID查找要移除的元素，这是最快最高效的方式
-    const elementToRemove = document.getElementById('menu-app');
+    // 定义一个包含所有要移除的元素ID的列表
+    const idsToRemove = ['menu-app', 'side_func'];
 
-    // 如果元素存在，就将其从父节点中移除
-    if (elementToRemove) {
-        elementToRemove.remove();
-        console.log('【油猴脚本】: 已成功移除“软媒应用”菜单。');
-    }
+    // 遍历列表，查找并移除每个元素
+    idsToRemove.forEach(id => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.remove();
+            console.log(`【IT之家 界面净化】: 已成功移除元素 #${id}。`);
+        }
+    });
+
 })();
